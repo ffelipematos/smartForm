0246eb756a45fda82629591f6503213c072759ce
noirtunnel <61060701+ffelipematos@users.noreply.github.com>
2020-03-05 16:59:32 -0300

    Add files via upload

---
+++ javascript_learning/js/index.js
@@ -0,0 +1,29 @@
+function getFormData($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
+
+$(document).ready(function(){
+    $("button").click(function(){
+      //var form = JSON.stringify($("form").serializeArray());
+
+     var form = getFormData($("form")); 
+
+      $.each(form, (function(key, val){
+          console.log(key,val);
+      }))
+      
+      console.log(form);
+    });
+
+    $('#inputCPF').mask('000.000.000-00');
+    $('#inputCEP').mask('0000-0000');
+
+  
+  });
